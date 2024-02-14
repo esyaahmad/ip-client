8ff88e815dac3da2b303dd1836f5cbe155877f36
esyaahmad <esya.ahmad@gmail.com>
2024-02-14 09:05:11 +0700

    login register client done

---
+++ individualP/src/pages/Login.jsx
@@ -0,0 +1,173 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import {
+    Card,
+    Input,
+    Checkbox,
+    Button,
+    Typography,
+  } from "@material-tailwind/react";
+
+export default function Login () {
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const navigate = useNavigate()
+    const url = 'http://localhost:3000'
+
+    async function handleLogin(e) {
+        e.preventDefault()
+        try {
+            const {data} = await axios.post(`${url}/login`, {email, password})
+            // console.log(data.access_token);
+            localStorage.setItem('access_token', data.access_token)
+            Swal.fire({
+                icon: "success",
+                title: `Logged in`
+            })
+            navigate('/')
+        } catch (error) {
+            console.log(error);
+            Swal.fire({
+                icon: "error",
+                title: error.response.data.message
+            })
+        }
+    }
+
+    return (
+        <>
+
+<section className="m-8 flex gap-4">
+      <div className="w-full lg:w-3/5 mt-0">
+        <div className="text-center">
+          <Typography variant="h2" className="font-bold mb-4">Sign In</Typography>
+          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Enter your email and password to Sign In.</Typography>
+        </div>
+        <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2" onSubmit={handleLogin}>
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+              Your email
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              onChange={(e) => setEmail(e.target.value)}
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+              Password
+            </Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="********"
+              onChange={(e) => setPassword(e.target.value)}
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+          </div>
+          
+          <Button type="submit"className="mt-6" fullWidth>
+            Sign In
+          </Button>
+
+          <div className="flex items-center justify-between gap-2 mt-6">
+            
+            <Typography variant="small" className="font-medium text-gray-900">
+              <a href="#">
+                Forgot Password
+              </a>
+            </Typography>
+          </div>
+          <div className="space-y-4 mt-8">
+            <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
+              <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_1156_824)">
+                  <path d="M16.3442 8.18429C16.3442 7.64047 16.3001 7.09371 16.206 6.55872H8.66016V9.63937H12.9813C12.802 10.6329 12.2258 11.5119 11.3822 12.0704V14.0693H13.9602C15.4741 12.6759 16.3442 10.6182 16.3442 8.18429Z" fill="#4285F4" />
+                  <path d="M8.65974 16.0006C10.8174 16.0006 12.637 15.2922 13.9627 14.0693L11.3847 12.0704C10.6675 12.5584 9.7415 12.8347 8.66268 12.8347C6.5756 12.8347 4.80598 11.4266 4.17104 9.53357H1.51074V11.5942C2.86882 14.2956 5.63494 16.0006 8.65974 16.0006Z" fill="#34A853" />
+                  <path d="M4.16852 9.53356C3.83341 8.53999 3.83341 7.46411 4.16852 6.47054V4.40991H1.51116C0.376489 6.67043 0.376489 9.33367 1.51116 11.5942L4.16852 9.53356Z" fill="#FBBC04" />
+                  <path d="M8.65974 3.16644C9.80029 3.1488 10.9026 3.57798 11.7286 4.36578L14.0127 2.08174C12.5664 0.72367 10.6469 -0.0229773 8.65974 0.000539111C5.63494 0.000539111 2.86882 1.70548 1.51074 4.40987L4.1681 6.4705C4.8001 4.57449 6.57266 3.16644 8.65974 3.16644Z" fill="#EA4335" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_1156_824">
+                    <rect width="16" height="16" fill="white" transform="translate(0.5)" />
+                  </clipPath>
+                </defs>
+              </svg>
+              <span>Sign in With Google</span>
+            </Button>
+            <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
+              <img src="/img/twitter-logo.svg" height={24} width={24} alt="" />
+              <span>Sign in With Twitter</span>
+            </Button>
+          </div>
+          <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
+            Not registered?
+            <Link to="/register" className="text-gray-900 ml-1">Create account</Link>
+          </Typography>
+        </form>
+
+      </div>
+      <div className="w-2/5 h-auto max-h-[660px] hidden lg:block">
+        <img
+          src="/src/assets/login.jpg"
+          className="h-full w-full object-cover rounded-3xl"
+        />
+      </div>
+
+    </section>
+
+
+        {/* <div className="relative flex flex-col justify-center h-screen overflow-hidden">
+  <div className="w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-lg">
+    <h1 className="text-3xl font-semibold text-center text-gray-700">
+      Login
+    </h1>
+    <form className="space-y-4" onSubmit={handleLogin}>
+      <div>
+        <label className="label">
+          <span className="text-base label-text">Email</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="w-full input input-bordered"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label className="label">
+          <span className="text-base label-text">Password</span>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          className="w-full input input-bordered"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <Link to='/register'>
+      <p
+        className="text-xs text-gray-600 hover:underline hover:text-blue-600"
+      >
+        Create Account Here
+      </p>
+      </Link>
+     
+      <div>
+        <button className="btn btn-block">Login</button>
+      </div>
+    </form>
+  </div>
+</div> */}
+
+        </>
+    )
+}
--- individualP/src/routers/index.jsx
+++ individualP/src/routers/index.jsx
@@ -1,12 +1,33 @@
 import {createBrowserRouter, redirect} from 'react-router-dom'
 import Swal from 'sweetalert2'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import Sidenav from '../../components/sidenav'
 
 
 const router = createBrowserRouter([
-{
-    path: "/",
-    element: <Home />,
+    {
+        path: '/register',
+        element: <Register/>
+    },
+    {
+        path: '/login',
+        element: <Login/>,
+        loader: () => {
+            if(localStorage.access_token) {
+                Swal.fire({
+                    icon: "warning",
+                    title: `You are Already Logged In`
+                })
+                return redirect('/')
+            }
+            return null
+        }
+    },
+    {
+        path:'/',
+    element: <Sidenav />
 }
 
 
